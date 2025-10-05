8cdb3da4ecadcc5df06c09e9da0de11bcb3dcaa0
Mayank Chauhan <124248806+Mayankk499@users.noreply.github.com>
2025-10-05 21:52:01 +0530

    feat(unpipe): add unpipe module with license, README, and implementation feat(vary): introduce vary module with license, README, and implementation feat(wrappy): add wrappy module with license, README, and implementation

---
+++ index.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import 'dotenv/config';
+
+const app = express();
+const PORT = process.env.PORT;
+
+app.get('/', (req, res) => {
+    return res.json({status: 'Server is up & running'});
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is listening on PORT ${PORT}`);
+})
